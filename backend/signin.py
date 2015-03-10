6e0d6967b1f31f5813f6b0ec1ec8ee56a0b9e73a
huangkq1989 <huangkq1989@gmail.com>
2015-03-10 23:45:04 +0800

    add some logic for sign and register

---
+++ backend/signin.py
@@ -0,0 +1,22 @@
+#!--*--coding:utf8--*--
+
+import hashlib
+import binascii
+import os
+
+#import mysql_python
+
+
+class SignInHelper(object):
+    """Handle Register by interacting with MySQL.
+    And sending an confirm email where handle a new register request.
+    """
+
+    @staticmethod
+    def check_valid(name, password):
+        return True
+
+
+if __name__=='__main__':
+    pass
+
